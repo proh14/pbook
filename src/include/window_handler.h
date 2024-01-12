b9311a678a93de05cecd0d1532d75fc387989d05
Hoorad <hourrad.f@gmail.com>
2024-01-13 10:06:30 +1300

    Added some other window handling functions

--- src/include/window_handler.h
+++ src/include/window_handler.h
@@ -6,5 +6,8 @@
 
 void init_window(WINDOW **win, WINDOW **subwin, int *rows, int *cols);
 void delete_window(WINDOW *win, WINDOW *subwin);
+void reformat(WINDOW *win, int w_cols);
+void middlePrint(WINDOW *win, int starty, int startx, int width,
+                 const char *string);
 
 #endif
--- src/window_handler.c
+++ src/window_handler.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <string.h>
 #include <window_handler.h>
 
 void init_window(WINDOW **win, WINDOW **subwindow, int *rows, int *cols) {
@@ -14,4 +15,36 @@ void init_window(WINDOW **win, WINDOW **subwindow, int *rows, int *cols) {
 void delete_window(WINDOW *win, WINDOW *subwindow) {
   delwin(subwindow);
   delwin(win);
+}
+
+void reformat(WINDOW *win, int w_cols) {
+  box(win, 0, 0);
+  mvwaddch(win, 2, 0, ACS_LTEE);
+  mvwaddch(win, 2, w_cols - 1, ACS_RTEE);
+  mvwhline(win, 2, 1, ACS_HLINE, w_cols - 2);
+  wrefresh(win);
+}
+
+// https://tldp.org/HOWTO/NCURSES-Programming-HOWTO
+// with modification
+void middlePrint(WINDOW *win, int starty, int startx, int width,
+                 const char *string) {
+  int length, x, y;
+  int temp;
+
+  if (win == NULL)
+    win = stdscr;
+  getyx(win, y, x);
+  if (startx != 0)
+    x = startx;
+  if (starty != 0)
+    y = starty;
+  if (width == 0)
+    width = 80;
+
+  length = strlen(string);
+  temp = (width - length) / 2;
+  x = startx + (int)temp;
+  mvwprintw(win, y, x, "%s", string);
+  refresh();
 }
